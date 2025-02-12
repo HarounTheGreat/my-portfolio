f8b8811f56278333725175b3013156d240953429
haroun <harounbouriga99@gmail.com>
2025-02-12 18:29:22 +0100

    update intro & add About Me ,expirence & Education

--- src/components/work/Work.jsx
+++ src/components/work/Work.jsx
@@ -1,14 +1,14 @@
-import React from 'react';
+import React from "react";
 import "./work.css";
-import Works from './Works';
+import Works from "./Works";
 const Work = () => {
   return (
-    <section className="work section" id='portfolio'>
-        <h2 className="section__title">Portfolio</h2>
-        <span className="section__subtitle">Most Recent Project</span>
-        <Works/>
+    <section className="work section" id="portfolio">
+      <h2 className="section__title"> Achievements</h2>
+      <span className="section__subtitle">Most Recent Achievements</span>
+      <Works />
     </section>
-  )
-}
+  );
+};
 
-export default Work
+export default Work;
